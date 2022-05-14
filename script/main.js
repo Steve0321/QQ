7fc9de987d0cc1925428682610a92e7f3198161a
Steve0321 <55421179+Steve0321@users.noreply.github.com>
2022-05-14 11:46:16 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -29,7 +29,22 @@ const animationTimeline = () => {
   // Spit chars that needs to be animated individually
   const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
   const hbd = document.getElementsByClassName("wish-hbd")[0];
-
+  function audioAutoPlay(id){
+    var audio = document.getElementById(id),
+      play = function(){
+        audio.play();
+        document.removeEventListener("touchstart",play, false);
+      };
+    audio.play();
+      document.addEventListener("WeixinJSBridgeReady", function () {
+          play();
+      }, false);
+      document.addEventListener('YixinJSBridgeReady', function() {
+        play();
+      }, false);
+      document.addEventListener("touchstart",play, false);
+  }
+  audioAutoPlay('Jaudio');
   textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
     .split("")
     .join("</span><span>")}</span`;
